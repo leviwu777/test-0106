58ce458aa7513dd22e632168e7662bbd00a762f7
Levi Wu <leviwork7@gmail.com>
2023-01-06 22:45:14 +0800

    update

--- script/git-hooks/pre-push.sh
+++ script/git-hooks/pre-push.sh
@@ -1,5 +1,3 @@
-echo 'Start pre-push check'
-
 remote=$1 # origin
 url=$2 # git@github.com:leviwu777/test-0106.git
 
@@ -30,10 +28,10 @@ current_branch=$(git rev-parse --abbrev-ref HEAD)
 pr_base_url="https://github.com/${org}/${project}/compare/$(parent_branch)...${current_branch}"
 pr_base_options="quick_pull=1&assignees=${github_id}"
 
-echo "\r\n"
+echo "\r"
 echo "\033[0;33m快速建立 Pull Request：\033[0m"
 echo "\033[41;37m[ Bugfix ]\033[0m: $pr_base_url?$pr_base_options&template=template_bugfix.md&labels=Bugfix"
 echo "\033[42;37m[ Feature ]\033[0m: $pr_base_url?$pr_base_options&template=template_feature.md&labels=Feature"
 echo "\033[42;37m[ Crawler ]\033[0m: $pr_base_url?$pr_base_options&template=template_crawler.md&labels=Feature"
 echo "\033[43;37m[ Experiment ]\033[0m: $pr_base_url?$pr_base_options&template=template_experiment.md&labels=Experiment"
-echo "\r\n"
+echo "\r"
