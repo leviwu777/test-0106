8c81e04f69dbb27ea1a234c4903c1c8f9c8df1bc
Levi Wu <leviwork7@gmail.com>
2023-01-06 22:44:33 +0800

    update

--- script/git-hooks/pre-push.sh
+++ script/git-hooks/pre-push.sh
@@ -17,14 +17,23 @@ project=${project%%.*}
 
 github_id="$(git config user.github-statementdog)"
 
-parent_branch=$(git log --pretty=format:'%D' HEAD^ | grep 'origin/' | head -n1 | sed 's@origin/@@' | sed 's@,.*@@')
+function parent_branch() {
+  local git_parent_branch
+  git_parent_branch=$(git log --pretty=format:'%D' HEAD^ | grep 'origin/' | head -n1 | sed 's@origin/@@' | sed 's@,.*@@')
+  if [[ -z "$git_parent_branch" ]]; then
+    echo "master"
+  else
+    echo "$git_parent_branch"
+  fi
+}
 current_branch=$(git rev-parse --abbrev-ref HEAD)
-pr_base_url="https://github.com/${org}/${project}/compare/${parent_branch}...${current_branch}"
+pr_base_url="https://github.com/${org}/${project}/compare/$(parent_branch)...${current_branch}"
 pr_base_options="quick_pull=1&assignees=${github_id}"
 
-echo "\r\n快速建立 Pull Request："
-echo "[ Bugfix ]: $pr_base_url?$pr_base_options&template=template_bugfix.md&labels=Bugfix"
-echo "[ Feature ]: $pr_base_url?$pr_base_options&template=template_feature.md&labels=Feature"
-echo "[ Crawler ]: $pr_base_url?$pr_base_options&template=template_crawler.md&labels=Feature"
-echo "[ Experiment ]: $pr_base_url?$pr_base_options&template=template_experiment.md&labels=Experiment"
+echo "\r\n"
+echo "\033[0;33m快速建立 Pull Request：\033[0m"
+echo "\033[41;37m[ Bugfix ]\033[0m: $pr_base_url?$pr_base_options&template=template_bugfix.md&labels=Bugfix"
+echo "\033[42;37m[ Feature ]\033[0m: $pr_base_url?$pr_base_options&template=template_feature.md&labels=Feature"
+echo "\033[42;37m[ Crawler ]\033[0m: $pr_base_url?$pr_base_options&template=template_crawler.md&labels=Feature"
+echo "\033[43;37m[ Experiment ]\033[0m: $pr_base_url?$pr_base_options&template=template_experiment.md&labels=Experiment"
 echo "\r\n"
